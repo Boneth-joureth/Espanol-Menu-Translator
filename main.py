d0bcb230463853f43962b9425e01c1f15b0a8f0a
Boneth-joureth <micah.bird@stargateschool.org>
2022-03-15 10:43:06 -0600

    changes

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #Importing Librarires
 from googletrans import Translator
-
 translator = Translator()
 
+# Variables
+inputWords = open("testWords.txt", "r")
+
 print(translator.translate('perro', dest='en').text)
+
+
+#Close files
+inputWords.close
