3982f26e35717e598932ccd749e56876dac33832
Strizerz <aadeshwins55@gmail.com>
2022-03-15 18:07:10 -0600

    att error

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ translator = Translator()
 # Variables
 inputWords = open("testWords.txt", "r")
 
+
 print(translator.translate('perro', dest='en').text)
 
 
@@ -13,3 +14,5 @@ inputWords.close
 
 
 #Aadesh testing to see if you can see this
+
+# I do not have time rn to test it but if you get an error "ATRIBUTE ERROR : NoneType object has no attribute 'group'" then check out this https://pytutorial.com/python-solve-err-googletrans
