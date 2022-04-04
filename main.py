c83986fadc7e5972e70726488bd5983d1237383c
Boneth-joureth <micah.bird@stargateschool.org>
2022-04-04 10:20:40 -0600

    YOOOO IT WORKS!!!!111!!

--- main.py
+++ main.py
@@ -4,12 +4,15 @@
 from ntpath import join
 from re import I
 from typing import List
-from googletrans import Translator as ts
+from googletrans import Translator
 
 #Open Necessary Files for Reading and Writing
 inputWords = open("testWords.txt", "r")##@@ change testwords.txt into input.txt when ready to test with your own input.
 outputWords = open("translated.txt", "w") #Store the translated words in a txt file so you can access it when needed
 outputWordsRead = open("translated.txt", "r")
+
+#init
+translator = Translator()
 
 #################################################### translates the words and puts them into another file neatly ####################################################
 
@@ -17,11 +20,10 @@ wordList = inputWords.read().split("\n")
 
 #Translates the words in wordList (Still doesn't work, IDK why?)
 for word in wordList:
-    word = ts.translate(word)
-
-#prints the now translated wordList to translated.txt
-for word in wordList:
+    word = translator.translate(text=word, src='es', dest='en').text
+    print(word)
     outputWords.write(word + "\n")
+    print('Writing to file...')
 
 #Closes files
 inputWords.close
