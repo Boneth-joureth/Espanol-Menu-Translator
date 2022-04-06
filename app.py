a4e11d6e09e41ac5c90400b13a0cc9c58fa00f85
Boneth-joureth <micah.bird@stargateschool.org>
2022-04-06 13:42:42 -0600

    Updates for flash

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
 
+myTest = "Epic"
+
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template("index.html")
