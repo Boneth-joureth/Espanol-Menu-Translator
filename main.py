e37db0cf8b5b33a494befa9447a47c36aeda4fa1
LAPTOP-A3IA6CIC\Justin <jvonr43@gmail.com>
2022-04-08 10:09:40 -0600

    Simplified yet a bit more

--- main.py
+++ main.py
@@ -4,18 +4,11 @@ from re import I
 from typing import List
 import translators as ts
 
-#Open Necessary Files for Reading and Writing
-inputFile = open("input.txt", "r")##@@ change testwords.txt into input.txt when ready to test with your own input.
-outputFile = open("output.txt", "w+") #Store the translated words in a txt file so you can access it when needed
-
 #Variable Definitions:
 #These may be helpful if it is necessary to switch y between a vowel and a consonant, also just needed to 
 #make things work correctly
 vowels = ['a', 'e', 'i', 'o', 'u','í','ó','ú','é']
 consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-
-inputVals = []
-outputVals = []
 
 #Function Definitions:
 
@@ -28,24 +21,23 @@ def CountVal(word):
         val += word.casefold().count(char)
     return val
 
-inputString = inputFile.read()
-inputWords = inputString.split('\n')
-outputWords = ts.google(inputString).split('\n')
+def Translate():
+    inputFile = open("input.txt", "r")
+    outputFile = open("output.txt", "w+")
 
-for word in inputWords:
-    inputVals.append(CountVal(word))
+    inputString = inputFile.read()
+    inputWords = inputString.split('\n')
+    outputWords = ts.google(inputString).split('\n')
 
-for word in outputWords:
-    outputVals.append(CountVal(word))
+    outputFile.write("Spanish: \n")
+    for word in inputWords:
+        outputFile.write(word + " " + str(CountVal(word)) + '\n')
+    outputFile.write("English: \n")
+    for word in outputWords:
+        outputFile.write(word + " " + str(CountVal(word)) + '\n')
 
-#Writes all results to the output file in a neat format:
-outputFile.write("Spanish: \n")
-for i in range(len(inputWords)):
-    outputFile.write(inputWords[i] + " " + str(inputVals[i]) + '\n')
-outputFile.write("English: \n")
-for i in range(len(outputWords)):
-    outputFile.write(outputWords[i] + " " + str(outputVals[i]) + '\n')
-    
-#Closes files
-inputFile.close
-outputFile.close
+    #Closes files
+    inputFile.close
+    outputFile.close
+
+Translate()
