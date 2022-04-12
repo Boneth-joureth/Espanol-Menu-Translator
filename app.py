d4e45cb09a28ae22e8b729f6bb33917372d41270
Boneth-joureth <micah.bird@stargateschool.org>
2022-04-12 10:44:46 -0600

    Wow wee it works!

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
-#import main
-from flask import Flask, render_template, flash
+from flask import Flask, request, jsonify, render_template
 
+# create the flask app
 app = Flask(__name__)
 
+# what html should be loaded as the home page when the app loads?
+@app.route('/')
+def home():
+    return render_template('app_frontend.html', prediction_text="")
 
+# define the logic for reading the inputs from the WEB PAGE, 
+# running the model, and displaying the prediction
+@app.route('/predict', methods=['GET','POST'])
+def predict():
 
-@app.route("/")
-def index():
-    global myTest
-    myTest = 'Our Mess'
-    return render_template("index.html", myWord=myTest)
+    # get the description submitted on the web page
+    a_description = request.form.get('myInput')
+    print(a_description)
+    return render_template('app_frontend.html', prediction_text=a_description)
+    #return 'Description entered: {}'.format(a_description)
 
-#rendering the HTML page which has the button
-@app.route('/json')
-def json():
-    return render_template('json.html')
+#@app.route('/prediction', methods=['GET', 'POST'])
+#def prediction():
+#    if request.method == 'POST':
+#        prediction_data = request.json
+#        print(prediction_data)
+#    return jsonify({'result': prediction_data})
 
-#background process happening without any refreshing
-@app.route('/background_process_test')
-def background_process_test():
-    print ("Hello")
-    return ("nothing")
+# boilerplate flask app code
+if __name__ == "__main__":
+    app.run(debug=True)
