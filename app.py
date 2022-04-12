496e8db6889cfe66e331f2f68777c6ab1ec5c771
Boneth-joureth <micah.bird@stargateschool.org>
2022-04-12 10:16:05 -0600

    JSON TEST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import main
+#import main
 from flask import Flask, render_template, flash
 
 app = Flask(__name__)
@@ -7,9 +7,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    def myTestFunc(input):
-        print(input)
-        return input
     global myTest
-    myTest = 'aiosduasoidausio'
+    myTest = 'Our Mess'
     return render_template("index.html", myWord=myTest)
+
+#rendering the HTML page which has the button
+@app.route('/json')
+def json():
+    return render_template('json.html')
+
+#background process happening without any refreshing
+@app.route('/background_process_test')
+def background_process_test():
+    print ("Hello")
+    return ("nothing")
